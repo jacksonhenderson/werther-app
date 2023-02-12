c8d84a0710ec2b74deb2498a8979081a8643267f
Jackson Henderson <121150195+jacksonhenderson@users.noreply.github.com>
2023-02-12 12:54:48 +0330

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar";
+import TodaysCard from "./Components/TodaysCard";
+
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <TodaysCard />
+        </>
+    );
+};
+
+export default App;
